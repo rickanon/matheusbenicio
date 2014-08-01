ba22559fa16b1a1d2698a70ba8098fe83b5e6497
rickanon <m4theus.benicio@gmail.com>
2014-08-01 19:15:59 -0300

    Create Arvore Genealógica.c

---
+++ Arvore Genealógica.c
@@ -0,0 +1,16 @@
+/* Estrutura de Dados - Arvore Genealógica */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <declaracao_funcoes.h>
+#include <funcoes_arvore.h>
+
+int main(){// Função que oferece as opções do programa
+		
+		raiz = NULL;//inicialização do ponteiro da raiz
+		menu();//Chamada do menu
+		free(raiz);//liberação da memória 
+	
+	return 0;
+	
+}
